5bf3f267107ddb10bcb338220b8787f249df8549
soto <victor.alonso@xmarts.com>
2019-04-25 14:27:40 -0500

    actualizacion
    se agrega formato al ticket en pos

--- modulo/duplicate/models/models.py
+++ modulo/duplicate/models/models.py
@@ -36,3 +36,22 @@ class amount_to_texts(models.Model):
 
 		self.no_piezas = sum(line.product_uom_qty for line in self.order_line)
 
+
+class pos_config_adds(models.Model):
+	_inherit = "pos.config"
+
+	direccion_ptv = fields.Char(string="Dirección de PTV")
+	telefono_ptv = fields.Char(string="Teléfono de PTV")
+
+class pos_order(models.Model):
+	_inherit = "pos.order"
+
+	amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True,
+								help='Amount of the invoice in letter')
+
+
+	@api.one
+	@api.depends('amount_total')
+	def _get_amount_to_text(self):
+		self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total)
+
